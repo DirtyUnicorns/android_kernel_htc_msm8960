c5f70cdf6260effd295a607625defc42456616af
Chester Kener <Cl3Kener@gmail.com>
2014-03-20 08:14:49 -0400

    Nightmare Gov
    Change-Id: I2486092a10a2c412c15d72e90f1387b932df18d7

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,6 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
 #endif
 
 
