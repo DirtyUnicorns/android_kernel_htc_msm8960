bdce97d31436596b8a611e02b9b6bc4fe48774c8
Chester Kener <Cl3Kener@gmail.com>
2014-03-20 08:29:10 -0400

    PegasusQ Gov
    Change-Id: I627ca1b77f60d7881c33ee3fe6624cbb21030847

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
 #endif
 
 
