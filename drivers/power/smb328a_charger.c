8af9394e46acd10c6d4544bd28164fcad098ba3d
Sultanxda <sultanxda@gmail.com>
2014-06-19 01:46:29 -0700

    smb328a_charger: Remove CHG_MODE_UNKNOWN
    CHG_MODE_UNKNOWN is detected as CHG_MODE_AC instead.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/power/smb328a_charger.c
+++ drivers/power/smb328a_charger.c
@@ -80,8 +80,7 @@ enum {
 	CHG_MODE_NONE,
 	CHG_MODE_AC,
 	CHG_MODE_USB,
-	CHG_MODE_MISC,
-	CHG_MODE_UNKNOWN
+	CHG_MODE_MISC
 };
 
 enum {
@@ -234,8 +233,7 @@ static void smb328a_set_command_reg(struct i2c_client *client)
 		dev_info(&client->dev, "%s : reg (0x%x) = 0x%x\n",
 			__func__, reg, data);
 		if (chip->chg_mode == CHG_MODE_AC ||
-			chip->chg_mode == CHG_MODE_MISC ||
-			chip->chg_mode == CHG_MODE_UNKNOWN)
+			chip->chg_mode == CHG_MODE_MISC)
 			data = 0xad;
 		else
 			data = 0xa9; /* usb */
@@ -1290,8 +1288,7 @@ static int smb328a_enable_charging(struct i2c_client *client)
 		dev_info(&client->dev, "%s : reg (0x%x) = 0x%x\n",
 					__func__, reg, data);
 		if (chip->chg_mode == CHG_MODE_AC ||
-			chip->chg_mode == CHG_MODE_MISC ||
-			chip->chg_mode == CHG_MODE_UNKNOWN)
+			chip->chg_mode == CHG_MODE_MISC)
 			data = 0xad;
 		else if (chip->chg_mode == CHG_MODE_USB)
 			data = 0xa9;
